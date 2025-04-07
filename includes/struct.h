02c70b9d9672a3ca8dab3dea74c7a7d4802baca8
Hamad <AlmarzooqiH23@gmail.com>
2025-04-07 06:51:49 +0400

    I have added onto the struct and renamed some variables.

--- includes/struct.h
+++ includes/struct.h
@@ -26,11 +26,29 @@
 # ifdef __APPLE__
 #include "../minilibx/mlx.h"
 # endif
+
+/**
+ * @brief This is where the window data will be stored at.abort
+ * @var	mlx The mlx connection to the mlx libarary.
+ * @var win The window data.
+ * @var	img This will hold the image pointer.
+ * @var	edn This will store if the system uses Small/Big endian.
+ * @var	imgd This will hold the image data. This variable will be used to mani-
+ * -pulate what is displayed.
+ * @var	bpp bits per pixel will be  filled  with  the  number of bits needed t-
+ * -represent a pixel color (also called the depth of the  image).
+ * @var	sl Size line is  the number  of  bytes  used to store one line of the
+ * image in memory.
+ */
 typedef	struct s_2d
 {
 	void	*mlx;
 	void	*win;
-	int		endian;
+	void	*img;
+	int		edn;
+	char	*imgd;
+	int		bpp;
+	int		sl;
 } t_d;
 
 #endif
