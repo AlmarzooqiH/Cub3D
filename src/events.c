222cde3b86023df13c9c95ad6c2a739161b52900
Hamad <AlmarzooqiH23@gmail.com>
2025-04-07 06:53:06 +0400

    I have added the mandatory key hooks which are ESC, W, A, S, D, Left Arrow Key and Right Arrow Key.

--- src/events.c
+++ src/events.c
@@ -6,7 +6,7 @@
 /*   By: hamad <hamad@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 05:37:34 by hamad             #+#    #+#             */
-/*   Updated: 2025/04/07 05:51:34 by hamad            ###   ########.fr       */
+/*   Updated: 2025/04/07 06:50:05 by hamad            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,17 @@ int	key_hook(int key_num, void *p)
 	pr = (t_d *)p;
 	if (key_num == ESC)
 		free_p(pr);
+	else if (key_num == W)
+		printf("W was pressed\n");
+	else if (key_num == A)
+		printf("A was pressed\n");
+	else if (key_num == S)
+		printf("S was pressed\n");
+	else if (key_num == D)
+		printf("D was pressed\n");
+	else if (key_num == LK)
+		printf("Left key was pressed\n");
+	else if (key_num == RK)
+		printf("Right key was pressed\n");
 	return (key_num);
 }
