c78e37e64606f93e93301fc03d32276097005e6f
Hamad <AlmarzooqiH23@gmail.com>
2025-04-09 01:17:43 +0400

    I have added several error messages.

--- includes/error.h
+++ includes/error.h
@@ -13,9 +13,16 @@
 #ifndef ERROR_H
 # define ERROR_H
 # include <stdio.h>
+
+/*  Input failure messages  */
+# define WRONG_INPUT "Input must be ./cub3D *.cub file.\n\0"
+# define INVALID_FILE "File must be *.cub file\n\0"
+# define FILE_DOESNT_EXSIST "File does not exists\n\0"
+
 /*  Memorey allocation failure messages.  */
 # define FTCM "Failed to calloc memorey\n\0"
-
+# define FTIT "Failed to init texture\n\0"
+# define FTIC "Failed to init color\n\0"
 /*      Mlx failure messages        */
 # define FTIM "Failed to init mlx\n\0"
 # define FTIW "Failed to init the window\n\0"
