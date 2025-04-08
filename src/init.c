98e0e415692c369cbf99dc976a285f41ee1a5042
Hamad <AlmarzooqiH23@gmail.com>
2025-04-09 01:19:18 +0400

    I have added the initalization of the t_texture and t_color structs and added init2 also.

--- src/init.c
+++ src/init.c
@@ -6,29 +6,62 @@
 /*   By: hamad <hamad@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 05:19:41 by hamad             #+#    #+#             */
-/*   Updated: 2025/04/07 07:52:40 by hamad            ###   ########.fr       */
+/*   Updated: 2025/04/09 01:14:18 by hamad            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
 
 /**
- * @brief This function will be responsible for the cleanup of the program.
- * @param	p the t_d struct. (Can be found in includes/struct.h)
- * @return (void)
+ * @brief This function will initalize the texture.
+ * @return t_texture pointer.
  */
-int	free_p(t_d *p)
+t_texture	*init_texture(void)
 {
-	if (p->img)
-		mlx_destroy_image(p->mlx, p->img);
-	if (p->mlx)
-		mlx_destroy_window(p->mlx, p->win);
-	if (p->mlx)
-		free(p->mlx);
-	if (p)
-		free(p);
-	exit(EXIT_SUCCESS);
-	return (1);
+	t_texture	*t;
+
+	t = (t_texture *)ft_calloc(1, sizeof(t_texture));
+	if (!t)
+		return (NULL);
+	t->width = 0;
+	t->height = 0;
+	return (t);
+}
+
+/**
+ * @brief This function will initalize the color.
+ * @return	t_color pointer.
+ */
+t_color	*init_color(void)
+{
+	t_color	*c;
+
+	c = ft_calloc(1, sizeof(t_color));
+	if (!c)
+		return (NULL);
+	c->r = 0;
+	c->g = 0;
+	c->b = 0;
+	return (c);
+}
+
+/**
+ * @brief This function will continue with the intialization of *t_d.
+ * @var	p	The *t_d pointer.
+ * @return (void).
+ */
+void	init2(t_d *p)
+{
+	p->floor = init_color();
+	p->ceiling = init_color();
+	if (!p->floor || !p->ceiling)
+	{
+		disp_err(FTIC);
+		free_p(p);
+		return ;
+	}
+	mlx_key_hook(p->win, key_hook, p);
+	mlx_hook(p->win, WIN_EXIT, 0, free_p, (void *)p);
 }
 
 /**
@@ -55,7 +88,12 @@ t_d	*init(void)
 	p->imgd = mlx_get_data_addr(p->img, &p->bpp, &p->sl, &p->edn);
 	if (!p->imgd)
 		return (disp_err(FTGID), free_p(p), NULL);
-	mlx_key_hook(p->win, key_hook, p);
-	mlx_hook(p->win, WIN_EXIT, 0, free_p, (void *)p);
+	p->e = init_texture();
+	p->w = init_texture();
+	p->s = init_texture();
+	p->n = init_texture();
+	if (!p->e || !p->w || !p->s || !p->n)
+		return (disp_err(FTIT), free_p(p), NULL);
+	init2(p);
 	return (p);
 }
