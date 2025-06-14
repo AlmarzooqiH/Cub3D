728ee23544a9dcc553b456a53ec5b5c537d28629
root <root@Hamad-Amarzooqi.localdomain>
2025-06-15 01:16:34 +0400

    Finally got the 3d working :) XD. Next is going to be textures and we will be 80% done 20% will remain due to bug search and fixing norminette and testing.

--- src/rendering_utils2.c
+++ src/rendering_utils2.c
@@ -6,20 +6,61 @@
 /*   By: hamalmar <hamalmar@student.42abudhabi.a    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/02 14:38:57 by hamalmar          #+#    #+#             */
-/*   Updated: 2025/06/11 22:03:23 by hamalmar         ###   ########.fr       */
+/*   Updated: 2025/06/15 01:13:18 by hamalmar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
 
-float	get_distance(t_player *p)
+/**
+ * @brief This function will return the eculidian distance from the player
+ * to the wall.
+ * @param player The player struct.
+ * @return The eculidian distance.
+ */
+float	get_distance(t_player *player)
 {
 	float	dx;
 	float	dy;
 
-	dx = p->ray->mapx - p->ppx;
-	dy = p->ray->mapy - p->ppy;
-	return ((dx * dx) + (dy * dy));
+	dx = player->ray->mapx - player->ppx;
+	dy = player->ray->mapy - player->ppy;
+	return (sqrtf(dx * dx + dy * dy));
+}
+
+/**
+ * @brief This function will draw the walls in 3d.
+ * @param p The program struct.
+ * @param ray_index The ray index that was sent.(Check raycast_in_2d())
+ * @return void
+ */
+void	draw_wall(t_d *p, int ray_index)
+{
+	float	corr;
+	float	proj;
+	int		end;
+	int		x;
+	int		y;
+
+	corr = p->player->ray->dist * cosf(p->player->ray->angle
+			- p->player->angle);
+	proj = (WIDTH / 2.0f) / tanf(dtor(60.0f) / 2.0f);
+	end = ((int)ceilf(proj / corr)) / 2 + HEIGHT / 2;
+	y = -((int)ceilf(proj / corr)) / 2 + HEIGHT / 2;
+	if (y < 0)
+		y = 0;
+	if (end >= HEIGHT)
+		end = HEIGHT - 1;
+	while (y <= end)
+	{
+		x = ray_index * (WIDTH / N_RAYS);
+		while (x < (ray_index * (WIDTH / N_RAYS)) + (WIDTH / N_RAYS))
+		{
+			put_pixel(p, x, y, ctoi(p->ceiling));
+			x++;
+		}
+		y++;
+	}
 }
 
 /**
@@ -28,7 +69,7 @@ float	get_distance(t_player *p)
  * @param The program structure.
  * @return void.
  */
-void	dda(t_d *p)
+void	dda(t_d *p, int ray_index)
 {
 	t_ray	*r;
 
@@ -49,9 +90,20 @@ void	dda(t_d *p)
 		{
 			r->hit = 1;
 			r->dist = get_distance(p->player);
+			draw_wall(p, ray_index);
 			draw_ray(p);
 		}
 	}
+}
+
+/**
+ * @brief D(egrees) To R(adinas)
+ * @param degrees The degree that you want to convert to radians.
+ * @return The converted degree to radians.
+ */
+float	dtor(float degree)
+{
+	return (degree * (M_PI / 180.0f));
 }
 
 /**
@@ -61,24 +113,23 @@ void	dda(t_d *p)
  */
 void	raycast_in_2d(t_d *p)
 {
-	float	start_angle;
-	float	end_angle;
-	float	step;
-	float	angle;
 	int		i;
+	float	sa;
+	float	ea;
+	float	stp;
+	float	ang;
 
-	start_angle = atan2(p->player->pdy, p->player->pdx)
-		- (30.0f * (M_PI / 180.0f));
-	end_angle = atan2(p->player->pdy, p->player->pdx)
-		+ (30.0f * (M_PI / 180.0f));
-	step = (end_angle - start_angle) / (N_RAYS - 1);
-	angle = start_angle;
+	sa = atan2f(p->player->pdy, p->player->pdx) - dtor(30.0f);
+	ea = atan2f(p->player->pdy, p->player->pdx) + dtor(30.0f);
+	stp = (ea - sa) / (N_RAYS - 1);
+	ang = sa;
 	i = 0;
 	while (i < N_RAYS)
 	{
-		update_ray(p->player, angle);
-		dda(p);
-		angle += step;
+		update_ray(p->player, ang);
+		p->player->ray->angle = ang;
+		dda(p, i);
+		ang += stp;
 		i++;
 	}
 }
